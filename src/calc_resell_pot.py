8ed61ab2b0f2e04daf0790302feef9120e073892
Andreas Oberdammer <andreas.oberdammer+coding@gmail.com>
2022-08-10 17:23:09 +0200

    added buffer

--- src/calc_resell_pot.py
+++ src/calc_resell_pot.py
@@ -8,6 +8,7 @@ SP_SELL_FEE_LOW_TIER = 0.12
 SP_SELL_FEE_HIGH_TIER = 0.06
 SP_BUY_FEES = 0.00
 ST_SELL_FEE = 0.15
+SELL_BUFFER = 0.97
 
 
 def calc_resell_pot(marketplace: str, buy_price: float, min_price: float, steam_price: float) -> list:
@@ -23,7 +24,7 @@ def calc_resell_pot(marketplace: str, buy_price: float, min_price: float, steam_
     else:
         sell_price = min_price * (1 - high_tier_fee)
 
-    strat_sell_price = steam_price * (1 - ST_SELL_FEE) * (1 - BUFF_SELL_FEE) * (1 - SP_SELL_FEE_HIGH_TIER)
+    strat_sell_price = steam_price * (1 - ST_SELL_FEE) * (1 - BUFF_SELL_FEE) * (1 - SP_SELL_FEE_HIGH_TIER) * SELL_BUFFER
 
     return buy_price, sell_price, strat_sell_price
 
